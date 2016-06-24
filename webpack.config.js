ffba5231862e5bf49ece1e362ae9fb2a62ed12b1
Simon Vrachliotis <simon@simonswiss.com>
2016-06-24 14:18:36 +1000

    removing `react` in the babel presets query in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel',
-        query: {presets: ['es2015', 'react']}
+        query: {presets: ['es2015']}
       },
       { 
         test: /\.jade$/,
